9879df58f0f4cb85405e91836c69801af5901b52
jose <jgabriel2608@gmail>
2024-01-04 12:13:54 -0500

    nuevos cambios

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo0411.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo0411.java
@@ -9,11 +9,14 @@ package ejemplos02;
  *
  * @author reroes
  */
-public class Ejemplo041 {
+public class Ejemplo0411 {
 
     public static void main(String[] args) {
         // 
-        int arreglo1[][] = {{1,2,3},{3,2,4},{2,6,2}};
+         // se invoca al método (procedimiento) obtenerSuma
+        
+    
+     int arreglo1[][] = {{1,2,3},{3,2,4},{2,6,2}};
         int arreglo2[][] = {{1,2,3},{2,2,2},{3,1,2}};
         int[][] arreglo3 = new int[3][3];
         int tabla;
@@ -22,19 +25,6 @@ public class Ejemplo041 {
                  tabla = obtenerMultiplicacion(arreglo1[i][j], arreglo2[i][j]);
                 arreglo3[i][j] = tabla;        
             }
-        }
-        for (int i = 0; i < arreglo1.length; i++) {
-            for (int j = 0; j < arreglo1[i].length; j++) {
-               System.out.printf("%d\t",arreglo3[i][j]);
-            }
-             System.out.println("\n");
-        }
-         System.out.println("\n");
-         for (int i = 0; i < arreglo2.length; i++) {
-             for (int j = 0; j < arreglo2[i].length; j++) {
-                   System.out.printf("%d\t",arreglo3[i][j]);
-             }
-             System.out.println("\n");
         }
          System.out.println("\n");
          for (int i = 0; i < arreglo3.length; i++) {
@@ -52,8 +42,6 @@ public class Ejemplo041 {
         
     }
 }
-
-
-    
-    
+        
+  
 
